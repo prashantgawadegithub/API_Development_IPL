cce808e362585e90f0a012bc41c28f25f3b42d09
Prashant Gawade <gawadeprashant97@gmail.com>
2023-07-08 15:18:29 +0530

    show two teams data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 import ipl
 
 app=Flask(__name__)
@@ -11,4 +11,15 @@ def home():
 def teams():
     teams=ipl.teamsAPI()
     return jsonify(teams)
+
+@app.route('/api/teamvteam')
+def teamvteam():
+    team1 = request.args.get('team1')
+    team2 = request.args.get('team2')
+
+    response=ipl.teamVteamAPI(team1,team2)
+    return jsonify(response)
+
+@app.route('')
+
 app.run(debug=True)
--- ipl.py
+++ ipl.py
@@ -11,3 +11,30 @@ def teamsAPI():
         'teams':teams
     }
     return team_dict
+
+def teamVteamAPI(team1,team2):
+
+    valid_teams=list(set(list(matches['Team1'])+list(matches['Team2'])))
+
+    if team1 in valid_teams and team2 in valid_teams:
+
+        temp_df=matches[(matches['Team1']==team1) & (matches['Team2']==team2) | (matches['Team1']==team2) & (matches['Team2']==team1)]
+        total_matches=temp_df.shape[0]
+
+        matches_won_team1=temp_df['WinningTeam'].value_counts()[team1]
+        matches_won_team2=temp_df['WinningTeam'].value_counts()[team2]
+
+        draws= total_matches - (matches_won_team1 + matches_won_team2)
+
+        response = {
+            'total_matches':str(total_matches),
+            team1: str(matches_won_team1),
+            team2: str(matches_won_team2),
+            'draws':str(draws)
+        }
+        return response
+
+    else:
+        return {'message':'invalid team name'}
+
+
